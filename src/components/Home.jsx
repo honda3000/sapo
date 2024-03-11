c0a92dd7cc8f021ee29947a570066be09c92c3af
honda3000 <agustin_6692@hotmail.com>
2024-03-11 11:13:09 -0300

    rana sound

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,19 @@ import header from "../assets/header.png";
 import { Link } from "react-scroll";
 import { CarouselCustomNavigation } from "./CarouselCustomNavigation";
 import { Button } from 'flowbite-react';
+import useSound from "use-sound";
+import ranaSound from "../assets/rana.mp3"
 
 const Home = () => {
+
+  const [useRanaSound]=useSound(ranaSound)
   return (
     <div className="items-center flex flex-col h-screen w-screen overflow-hidden bg-slate-400" style={{backgroundColor: "#168C16"}}>
       
       
       <CarouselCustomNavigation
 ></CarouselCustomNavigation>
-     <Button color="info" style={{margin:"20px",width:"200px",height:"100px"}}>BUY</Button>
+     <Button onClick={useRanaSound} color="info" style={{margin:"20px",width:"200px",height:"100px"}}>BUY</Button>
 
       
     </div>
